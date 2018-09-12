625f628ef89200844eb58cf1b595f10372245253
Naveenraj M <naveenrajm97@gmail.com>
2018-09-12 16:39:48 +0530

    Add files via upload

---
+++ examples/student.py
@@ -0,0 +1,77 @@
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.models import BayesianModel
+
+# creating bayesian objects
+difficulty_cpd = TabularCPD(variable='D',
+                       variable_card=2,
+                       values=[[.6, .4]])
+
+intelligence_cpd = TabularCPD(variable='I',
+                       variable_card=2,
+                       values=[[.7, .3]])
+
+sat_cpd = TabularCPD(variable='S',
+                     variable_card=2,
+                     values=[[.95, 0.2],
+                             [.05, 0.8]],
+                     evidence=['I'],
+                     evidence_card=[2])
+
+# grade
+grade_cpd = TabularCPD(variable='G',
+                         variable_card=3,
+                         values=[[.3, .05, .9, .5 ],
+                        [.4, .25, .08, .3],
+                        [.3, .7, .02, .2]],
+                         evidence=['I', 'D'],
+                         evidence_card=[2, 2])
+
+letter_cpd = TabularCPD(variable='L',
+                     variable_card=2,
+                     values=[[.1, 0.4, .99],
+                             [.9, 0.6, .01]],
+                     evidence=['G'],
+                     evidence_card=[3])
+
+# buildind model
+student_model = BayesianModel([('D', 'G'),('I', 'G'), ('I', 'S'), ('G', 'L')])
+
+# adding cpds
+student_model.add_cpds(difficulty_cpd, intelligence_cpd, sat_cpd, grade_cpd, letter_cpd)
+
+
+model_name = "student"
+# json dump part
+import json
+
+data = {    "name"              : "student",
+            "short_description" : "give short desc here",
+            "long_description"  : "give long desc here",
+            "nodes"           : {"L":"Letter", "D":"Difficulty", "G":"Grade", "I":"Intelligence", "S":"SAT Scores"}
+        }
+
+with open('./data/model.json', 'r+') as f:
+    model_json = json.load(f)
+    model_list = model_json["list"]
+
+    if model_name not in model_list:
+        model_list.append[model_name]
+
+    model_json["list"] = model_list
+
+    f.seek(0)        # <--- should reset file position to the beginning.
+    json.dump(model_json, f, indent=4)
+    f.truncate()
+
+
+with open('./data/student.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
+
+
+
+# pickle dump part
+import pickle
+
+pickle_out = open("./models/student.pickle","wb")
+pickle.dump(student_model, pickle_out)
+pickle_out.close()
---
+++ main.py
@@ -0,0 +1,124 @@
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.models import BayesianModel
+import json
+import pickle
+
+
+def list():
+    '''
+    Returns all models
+    '''
+    filename = './examples/data/model.json'
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    return data
+
+
+def model_present(model_name):
+    '''
+    checks a model present or not
+    '''
+    filename = './examples/data/model.json'
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    model_list = data["list"]
+    if model_name in model_list:
+        return True
+    else:
+        return False
+
+
+def describe(model_name):
+    '''
+    '''
+
+    if not model_present(model_name):
+        return "Model not found"
+
+    # getting model pickle
+    model_pickle = "./examples/models/"+model_name+".pickle"
+    pickle_in = open(model_pickle, "rb")
+    model = pickle.load(pickle_in)
+
+    # getting model mapper
+    model_json_file = "./examples/data/"+model_name+".json"
+    with open(model_json_file) as f:
+        model_json = json.load(f)
+
+
+    root = []
+    for x in model.in_degree:
+        if x[1] == 0:
+            root.append(x[0])
+    leaf = []
+    for x in model.out_degree:
+        if x[1] == 0:
+            leaf.append(x[0])
+
+    for i in root:
+        for j in leaf:
+            #to remove leaf nodes for which some of the root nodes have no influence
+            if str(model.is_active_trail(i,j))=='False':
+                leaf.remove(j)
+    Root=[]
+    Leaf=[]
+    m = model_json["nodes"]
+    for r in root:
+        for r1 in m:
+            if r==r1:
+                Root.append(m[r])
+    for l in leaf:
+        for l1 in m:
+            if l==l1:
+                Leaf.append(m[l])
+    if len(Leaf)>1:
+        op="the Leaves"
+    else:
+        op="the  Leaf"
+    if len(Root)>1:
+        op1="the Roots"
+    else:
+        op1="the Root"
+
+
+    s=""
+    last=" states,"
+    rlen=len(root)
+    for r,r1 in zip(root,Root):
+
+        length=model.get_cardinality(r)
+        if rlen!=1:
+            if rlen==2:
+                last=" states"
+            s+=str(r1)+" has "+ str(length) +last
+        else:
+            s+=first+str(r1)+" has "+ str(length) +last
+        rlen=rlen-1
+        if rlen==1:
+            first=" and "
+            last=" states."
+
+    s2=""
+    last2=" states,"
+    leaflen=len(leaf)
+    for l,l1 in zip(leaf,Leaf):
+
+        length2=model.get_cardinality(l)
+        if leaflen==1:
+            last2=" states"
+            first=""
+        if leaflen!=1:
+            if leaflen==2:
+                last2=" states"
+            s2+=str(l1)+" has "+ str(length2) +last2
+        else:
+            s2+=first+str(l1)+" has "+ str(length2) +last2
+        leaflen=leaflen-1
+        if leaflen==1:
+            first=" and "
+            last=" states."
+
+
+    return ("The "+model_name +" model "  " has "+str(Root).strip('[]')+" as " +op1+ " and "+str(Leaf).strip('[]')+" as "+op+". " +s+s2)
